d954eee03cf97ae1a34bc5614a89b3a016a67e88
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-05-11 16:36:14 -0300

    fix: tentativa de arrumar a imagem 2.6

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 export const Main = () => {
     return (
-        <main className="p-8 max-w-5xl m-auto-screen">
+        <main className="p-8 max-w-5xl m-auto h-screen">
             <section>
                 <div className="flex items-center flex-col justify-center h-full m-auto">
                     <div className="mt-8">
@@ -16,7 +16,7 @@ export const Main = () => {
 
                         <p className="text-2xl">Como um desenvolvedor front-end que vive e respira código, estou constantemente em busca da próxima grande aventura no mundo da programação. Com habilidades que abrangem desde JavaScript e Typescript nos frameworks React e Angular, além de todas aquelas bibliotecas e ferramentas que mantêm nosso ritmo no desenvolvimento moderno. Eu tenho um amor especial por manter meu código limpo e organizado, chega a tranquilizar a mente. Quando não estou imerso no código, estou dedicado a melhorar a comunicação da equipe e sempre buscando aprender novas tecnologias.</p>
 
-                        <p className="text-2xl">
+                        <p className="text-2xl mb-8">
                             O Luis que vai além da programação é uma pessoa que gosta desafios, independente de quais sejam, gosta de rir, estar com os amigos, treinar pesado, jogar xadrez, de passar tempo com seus cachorros e sua esposa. É também uma pessoa super competitiva, daquelas que perde as estribeiras jogando War (não me orgulho disto kkkk mas é a vida) e que também tenta levar a vida de maneira leve, fazendo piadas com as situações para evitar que o clima pese.
                         </p>
                     </div>
